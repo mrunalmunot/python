e18962aef2f24627f2cb5d64f869d13081492172
Mrunal Munot <67384965+mrunalmunot@users.noreply.github.com>
2022-09-07 22:41:04 +0530

    Create  decorator-3.py

---
+++ Decorators/decorator-3.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+#Define and implement bold_tag
+def bold_tag(func):
+    def inner(*args, **kwdargs):
+        return '<b>'+ '{}'.format(*args) +'</b>'
+    return inner
+
+@bold_tag
+def say(msg):
+    return msg
+    
+'''Check the Tail section for input/output'''
+if __name__ == "__main__":
+    with open(os.environ['OUTPUT_PATH'], 'w') as fout:
+        res_lst = list()
+        res_lst.append(say(input()))
+        fout.write("{}".format(*res_lst))
