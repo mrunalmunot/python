ef40b73366d43f5bfe2a48a84f04a34d3108f5f9
Mrunal Munot <67384965+mrunalmunot@users.noreply.github.com>
2022-09-07 22:41:50 +0530

    Create  decorator-5.py

---
+++ Decorators/decorator-5.py
@@ -0,0 +1,26 @@
+import os
+import sys
+def bold_tag(func):
+    
+    def inner(*args, **kwdargs):
+        return '<b>'+func(*args, **kwdargs)+'</b>'
+        
+    return inner
+
+def italic_tag(func):
+    
+    def inner(*args, **kwdargs):
+        return '<i>'+func(*args, **kwdargs)+'</i>'
+        
+    return inner
+    
+#Add greet() function definition
+@italic_tag
+def greet():
+    return input()
+'''Check the Tail section for input/output'''
+if __name__ == "__main__":
+    with open(os.environ['OUTPUT_PATH'], 'w') as fout:
+        res_lst = list()
+        res_lst.append(greet())
+        fout.write("{}".format(*res_lst))
