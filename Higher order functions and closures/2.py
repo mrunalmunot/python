0f03748e527f851da3c87c132bd4b3e8dd74e749
Mrunal Munot <67384965+mrunalmunot@users.noreply.github.com>
2022-09-08 11:51:17 +0530

    Update 2.py

--- Higher order functions and closures/2.py
+++ Higher order functions and closures/2.py
@@ -1,1 +1,25 @@
-import abc
+#!/bin/python3
+
+import sys
+import os
+
+
+
+# Add the factory function implementation here
+def factory(n):
+    def current():
+        return n
+    def counter():
+        return n+1
+
+    return current, counter
+    
+f_current, f_counter = factory(int(input()))
+
+if __name__ == "__main__":
+    with open(os.environ['OUTPUT_PATH'], 'w') as fout:
+        func_lst = [f_current, f_counter]
+        res_lst = list()
+        for func in func_lst:
+            res_lst.append(func())
+        fout.write("{}\n{}".format(*res_lst))
