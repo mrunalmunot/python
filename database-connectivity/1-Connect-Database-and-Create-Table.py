1fb8ad73fc1266b482e787aa369b0cc57eeacdc5
Mrunal Munot <67384965+mrunalmunot@users.noreply.github.com>
2022-09-08 10:20:57 +0530

    Update 1-Connect-Database-and-Create-Table.py

--- database-connectivity/1-Connect-Database-and-Create-Table.py
+++ database-connectivity/1-Connect-Database-and-Create-Table.py
@@ -1,1 +1,41 @@
-import abc
+#!/bin/python3
+
+import sys
+import os
+import sqlite3
+
+
+
+
+# Complete the following function:
+
+def main():
+    conn = sqlite3.connect('SAMPLE.db')
+    #create connection cursor
+    cursor = conn.cursor()
+    #create table ITEMS using the cursor
+    sql = """CREATE TABLE ITEMS (
+            ITEM_ID INT, 
+            ITEM_NAME VARCHAR, 
+            ITEM_DESCRIPTION VARCHAR, 
+            ITEM_CATEGORY VARCHAR, 
+            QUANTITY_IN_STOCK INT
+            )
+    """
+    cursor.execute(sql)
+    #commit connection 
+    conn.commit()
+    #close connection
+    conn.close()
+
+
+'''To test the code, no input is required'''
+
+if __name__ == "__main__":
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    res = main();
+    f.write(str(res) + "\n")
+
+
+    f.close()
