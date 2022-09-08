d09a16244de05a19805498208785c46e674ff444
Mrunal Munot <67384965+mrunalmunot@users.noreply.github.com>
2022-09-08 14:56:47 +0530

    Update 1.py

--- Descriptors/1.py
+++ Descriptors/1.py
@@ -1,1 +1,31 @@
-import abc
+#!/bin/python3
+
+import sys
+import os
+
+
+
+# Add Celsius class implementation below.
+class Celsius:
+    def __get__(self, owner, obj):
+        return (owner.fahrenheit-32)*5/9
+    
+    def __set__(self, owner, value):
+        owner.fahrenheit = 32+9*value/5
+
+# Add temperature class implementation below.        
+class Temperature:
+    celsius = Celsius()
+    def __init__(self, fahrenheit):
+        self.fahrenheit = fahrenheit
+        
+'''Check the Tail section for input/output'''
+
+if __name__ == "__main__":
+    with open(os.environ['OUTPUT_PATH'], 'w') as fout:
+        res_lst = list()
+        t1 = Temperature(int(input()))
+        res_lst.append((t1.fahrenheit, t1.celsius))
+        t1.celsius = int(input())
+        res_lst.append((t1.fahrenheit, t1.celsius))
+        fout.write("{}\n{}".format(*res_lst))
